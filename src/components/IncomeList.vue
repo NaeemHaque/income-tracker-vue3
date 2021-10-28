6f7d4fc6861e50b99fe79294838087ac2793fb9f
NaemHaque <naeemhaque248@gmail.com>
2021-10-28 14:26:54 +0600

    first commit

---
+++ src/components/IncomeList.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "IncomeList"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/IncomeListItem.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "IncomeListItem"
+}
+</script>
+
+<style scoped>
+
+</style>
